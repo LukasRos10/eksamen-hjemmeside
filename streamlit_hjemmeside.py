cee9a46381ddc3e4e6eafdda4e04240b229d780e
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-04 12:28:45 +0000

    ændrede titel

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -1,10 +1,9 @@
 from filecmp import clear_cache
-
 import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Velkommen til eksamen", )
 
-st.title("Velkommen til vores eksmamen i _____")
-st
+st.title("Velkommen til vores eksamen i _____")
+
 st.write("Vi har lavet denne hjemmeside for at præsentere vores eksamen i _____, hvor vi har arbejdet med _____ og _____.")
