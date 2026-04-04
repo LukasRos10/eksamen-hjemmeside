581daf75fbb1edd1ed711ded317e87d35f8340e5
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-04 12:13:14 +0000

    tilføjede titel og tekst

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -6,5 +6,5 @@ import pandas as pd
 st.set_page_config(page_title="Velkommen til eksamen", )
 
 st.title("Velkommen til vores eksmamen i _____")
-st.fontsize(14)
+st
 st.write("Vi har lavet denne hjemmeside for at præsentere vores eksamen i _____, hvor vi har arbejdet med _____ og _____.")
