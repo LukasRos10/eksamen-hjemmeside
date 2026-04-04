a3bc0f1363133490b42eb159642789d6c73ada95
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-04 11:47:39 +0000

    added a title to the exam page

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -5,5 +5,5 @@ import pandas as pd
 
 st.set_page_config(page_title="Velkommen til eksamen", )
 
-st.title("Velkommen til eksamen") 
-st.subtitle("Denne hjemmeside vil vi bruge som vores produkt igennem hele præsentationen, endnu engang velkommen til")
+st.markdown
+#titel("Velkommen til vores eksamen")
