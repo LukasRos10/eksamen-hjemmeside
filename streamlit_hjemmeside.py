0a654ed3e7523f65356025c5e8f8222c9ceb263b
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-04 20:53:34 +0000

    tilføjede billeder

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -22,13 +22,14 @@ st.image("https://newyorkerbyheart.dk/wp-content/uploads/2022/04/gammeldags-pand
 with col2:
     st.markdown("- [Kilde 2](https://www.kilde2.com)")
 #sæt et billede ind i kolonne 2
-st.image("https://image2url.com/r2/default/images/1775306768660-aafd1dac-cffe-4578-b067-9e3136c68ede.jpg", width=90)
+st.image("https://image2url.com/r2/default/images/1775306768660-aafd1dac-cffe-4578-b067-9e3136c68ede.jpg", width=100)
 
 with col3:
     st.markdown("- [Kilde 3](https://www.kilde3.com)")
-
+st.image("https://image2url.com/r2/default/images/1775335870643-37b4fa95-4458-4c6a-ac08-98fe15c49567.webp", width=100)
 with col4:
     st.markdown("- [Kilde 4](https://www.kilde4.com)")
-
+st.image("https://image2url.com/r2/default/images/1775335935496-b86386d9-32af-4ede-a2f7-2d8bf44f197c.jpg", width=100)
 with col5:
     st.markdown("- [Kilde 5](https://www.kilde5.com)")
+st.image("https://image2url.com/r2/default/images/1775336000578-8b83a6f9-c42f-4746-a18f-05f24a7fd2a1.jpg", width=100)
