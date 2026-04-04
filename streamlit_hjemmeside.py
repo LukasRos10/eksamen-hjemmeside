8d44dceeeeef2dabb0aec8bf48952ec5f817acba
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-04 12:39:55 +0000

    tilføjede plads til problemformulering og kilder

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -7,3 +7,25 @@ st.set_page_config(page_title="Velkommen til eksamen", )
 st.title("Velkommen til vores eksamen i _____")
 
 st.write("Vi har lavet denne hjemmeside for at præsentere vores eksamen i _____, hvor vi har arbejdet med _____ og _____.")
+
+
+st.write("Vores problemformulering er _________")
+
+
+st.write("Her er vores kildeliste:")
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    st.markdown("- [Kilde 1](https://www.kilde1.com)")
+
+with col2:
+    st.markdown("- [Kilde 2](https://www.kilde2.com)")
+
+with col3:
+    st.markdown("- [Kilde 3](https://www.kilde3.com)")
+
+with col4:
+    st.markdown("- [Kilde 4](https://www.kilde4.com)")
+
+with col5:
+    st.markdown("- [Kilde 5](https://www.kilde5.com)")
