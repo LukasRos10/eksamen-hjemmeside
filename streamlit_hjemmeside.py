15f22ecb3330ba991ed55c35abda7fd0f46eb527
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-04 21:48:23 +0000

    tilføjede dialog fordi jeg ikke ved hvad det er

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 st.set_page_config(page_title="Velkommen til eksamen", )
 
 st.date_input("Dato for eksamen:", value=datetime.date.today())
-
+st.dialog("Velkommen til eksamen i _____")
 st.title("Velkommen til vores eksamen i _____")
 
 st.write("Vi har lavet denne hjemmeside for at præsentere vores eksamen i _____, hvor vi har arbejdet med _____ og _____.")
