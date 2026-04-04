147b42f6a861d13fc63d7464cdf4801775bccf3d
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-04 11:41:24 +0000

    added title and subtitle

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -4,3 +4,6 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Velkommen til eksamen", )
+
+st.title("Velkommen til eksamen") 
+st.subtitle("Denne hjemmeside vil vi bruge som vores produkt igennem hele præsentationen, endnu engang velkommen til")
