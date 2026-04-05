32678b2b16c13dd4ff17d8317199fa737b884699
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-04-05 21:02:01 +0000

    fjernede docx

--- streamlit_hjemmeside.py
+++ streamlit_hjemmeside.py
@@ -60,7 +60,7 @@ with col3:
     st.write("Vi fandt ud af at mange medlemmer er utilfredse med folkekirken og derfor overvejer at melde sig ud")
 
 
-import docx
+
 st.header("Chatbot der selv læser og omskriver kildemateriale")
 
 # Funktion til at læse Word-dokument
